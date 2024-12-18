fb277f5c6a5ccaa8e020ba2b23c33defd315ee48
HarshaBhat24 <harshabhat666@gmail.com>
2024-12-18 15:34:22 +0530

    Unwanted removed

--- back-python/randomf.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import SelectFromModel
-from sklearn import cross_decomposition
-data=pd.read_csv('https://raw.githubusercontent.com/mburakergenc/Malware-Detection-using-Machine-Learning/refs/heads/master/data.csv ')
-legit_train,legit_test,mal_train,mal_test=cross_decomposition.train_test_split(data.drop(['hash','classification'], axis=1).values,data['classification'].values,test_size=0.2)
